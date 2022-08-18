511b7bdec94c9b368acb860c1d2e4bae080e3d09
Van Jason Crews <vanjasoncrews@gmail.com>
2022-08-18 14:03:23 -0400

    completed files

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-console.log("Let's get this party started!");
+async function searchGiphy(q) {
+  const getImage = document.querySelector('#imageHolder');
+  const res = await axios.get(
+    `https://api.giphy.com/v1/gifs/search?api_key=OgpBJdchr9guqSd0bMzAIP6gsMZ04IJT&q=${q}&limit=1&offset=0&rating=r&lang=en`
+  );
+  for (let i = 0; i < res.data.data.length; i++) {
+    console.log(res.data.data[i].images.original.url);
+    const newImage = document.createElement('img');
+    getImage.append(newImage);
+    newImage.src = res.data.data[i].images.original.url;
+    newImage.className = 'giphyImages';
+    // console.log(newImage.src);
+  }
+}
+const txtSearch = document.querySelector('#txtSearch');
+const searchBtn = document.querySelector('#btnSearch');
+searchBtn.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  searchGiphy(txtSearch.value);
+  //   alert();
+});
+
+const clearBtn = document.querySelector('#btnClear');
+clearBtn.addEventListener('click', function (evt) {
+  const getImages = document.querySelectorAll('.giphyImages');
+  evt.preventDefault();
+  getImages.forEach((image) => {
+    image.remove();
+  });
+});
